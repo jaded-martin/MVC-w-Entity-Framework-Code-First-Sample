2a414713abd53356bf708c20f7da740cdf1c8689
jaded-martin <jaded.martin@gmail.com>
2016-03-28 22:04:37 +0800

    solution files

---
+++ CodeFirstSample.WebApp/CodeFirstSample/Controllers/PeopleController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirstSample.Models;
+
+namespace CodeFirstSample.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private PeopleContext db = new PeopleContext();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Your list of Contacts...";
+            return View(db.Peoples.ToList());
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            People people = db.Peoples.Find(id);
+
+            ViewBag.Title = "Here is " + people.FirstName + "'s Info";
+
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            ViewBag.Title = "Got new friend?";
+            return View();
+        }
+
+        // POST: People/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,ContactNumber")] People people)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Peoples.Add(people);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(people);
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            People people = db.Peoples.Find(id);
+
+            ViewBag.Title = "Did " + people.FirstName + " changed number?";
+
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,ContactNumber")] People people)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(people).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(people);
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            People people = db.Peoples.Find(id);
+
+            ViewBag.Title = "Got a quarrel with " + people.FirstName + " or something?";
+
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            People people = db.Peoples.Find(id);
+            db.Peoples.Remove(people);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CodeFirstSample.WebApp/CodeFirstSample/Models/PeopleContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CodeFirstSample.Models
+{
+    public class PeopleContext : DbContext
+    {
+        public DbSet<People> Peoples { get; set; }
+    }
+}
